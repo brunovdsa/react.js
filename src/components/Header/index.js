e5d78f7c8eded08ce2d562f5d59416f87135e3e8
Bruno Vinícius Souza de Sá <brunoviniciussouza.sa@gmail.com>
2020-03-07 11:11:49 -0300

    Final Commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+export default Header;
+
+//"base_url": "https://rocketseat-node.herokuapp.com/api"
---
+++ src/services/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const Api = axios.create({baseURL: 'https://rocketseat-node.herokuapp.com/api'});
+
+export default Api;
